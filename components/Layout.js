b05ac995edc21b90bf24f605d5e5e1cbd147435a
Muhammad Nur Al Ghazali <muhammadnuralghazali@gmail.com>
2021-10-27 14:54:37 +0700

    Make Layout
    ~ Add Navbar
    ~ Add Layout
    ~ Add Footer

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head'
+import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>Next Mahakam</title>
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>mahakam</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        {children}
+      </Container>
+      <footer>
+        <Typography>
+          All rights reserved. Next Mahakam
+        </Typography>
+      </footer>
+    </div>
+  )
+}
