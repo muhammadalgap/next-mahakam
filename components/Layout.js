349fd99302d5d80250bdb11b9e8c92b63e919130
Muhammad Nur Al Ghazali <muhammadnuralghazali@gmail.com>
2021-10-27 16:33:49 +0700

    Add Style
    ~ Style Navbar
    ~ Style Body
    ~ Style Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
+import useStyles from '../utils/style'
 
 export default function Layout({ children }) {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Next Mahakam</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>mahakam</Typography>
+          <Typography><b>mahakam</b></Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
-      <footer>
+      <footer className={classes.footer}>
         <Typography>
           All rights reserved. Next Mahakam
         </Typography>
