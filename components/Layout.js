14bfaeea9ebae8a2bcf55f293b65cd7a85534872
Muhammad Nur Al Ghazali <muhammadnuralghazali@gmail.com>
2021-10-28 19:24:18 +0700

    Add-Material-UI-Theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,48 +1,76 @@
 import React from 'react'
 import Head from 'next/head'
 import NextLink from 'next/link'
-import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Container, Link, ThemeProvider, CssBaseline, createTheme } from '@material-ui/core'
 import useStyles from '../utils/style'
 
 export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      body1: {
+        fontWeight: 'normal',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const classes = useStyles();
-
   return (
     <div>
       <Head>
         <title>{title ? `${title} - Next Mahakam` : 'Next Mahakam'}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}><b>mahakam</b></Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
               <Link>
-                Cart
+                <Typography className={classes.brand}><b>mahakam</b></Typography>
               </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>
-                Login
-              </Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>
-        {children}
-      </Container>
-      <footer className={classes.footer}>
-        <Typography>
-          All rights reserved. Next Mahakam
-        </Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  Cart
+                </Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>
+                  Login
+                </Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>
+          {children}
+        </Container>
+        <footer className={classes.footer}>
+          <Typography>
+            All rights reserved. Next Mahakam
+          </Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
